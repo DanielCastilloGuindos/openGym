df73060747b01fec513d5f8ab5197ac6d428ad8c
Daniel Castillo <danielcastilloguindos@gmail.com>
2026-09-09 16:49:32 +0200

    feat: add Templates button to Plan routines header

--- frontend/src/views/Plan.jsx
+++ frontend/src/views/Plan.jsx
@@ -81,7 +81,10 @@ export default function Plan() {
     </div><div>
       <div className="row between" style={{ marginTop: 22, marginBottom: 10 }}>
         <h4 className="sec" style={{ margin: 0 }}>{t('Routines')}</h4>
-        <Button size="sm" variant="tinted" icon="plus" onClick={addRoutine}>{t('New')}</Button>
+        <div className="row" style={{ gap: 6 }}>
+          <Button size="sm" variant="tinted" icon="sparkles" onClick={starterPlanSheet}>{t('Templates')}</Button>
+          <Button size="sm" variant="tinted" icon="plus" onClick={addRoutine}>{t('New')}</Button>
+        </div>
       </div>
       {S.routines.length ? <div className="list">{S.routines.map(r => <div key={r.id} className="item" {...tappable(() => nav('/plan/r/' + r.id))}>
         <span className="lrow-i"><Icon name={glyphOf(r.emoji)} /></span>
